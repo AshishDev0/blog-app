6c7e165ead94c7396126d333f419b5ca80b1a74f
Ashish Chowdhary <ashish.chowdhary@6degree.co>
2025-05-07 14:15:09 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,30 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "gear5world.com",
+        protocol: "https",
+        port: ""
+      },
+      {
+        hostname: "lh3.googleusercontent.com",
+        protocol: "https",
+        port: ""
+      },
+      {
+        hostname: "s.yimg.com",
+        protocol: "https",
+        port: ""
+      },
+      {
+        hostname: "wallpapers-clan.com",
+        protocol: "https",
+        port: ""
+      },
+    ]
+  }
 };
 
 export default nextConfig;
